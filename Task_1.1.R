f640a5409757ff76b3013a56da3305ad47399a0f
biplopXbillionaire <biploprai25@gmail.com>
2026-03-12 16:14:34 +0800

    Add files via upload

---
+++ Task_1.1.R
@@ -0,0 +1,4 @@
+# Task 1.1 Generate your first R Script file, plot a histogram for a built-in dataset. save and execute code. 
+
+v <- c(9,13,21,8,36,22,12,41,31,33,19)
+hist(v,xlab = "Weight", col = "green", border = "blue")
